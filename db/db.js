b842ca215ad874378d3caf5d338fc665124ae938
Suvam Swagatam panda <suvampandar@gmail.com>
2023-05-12 14:30:42 +0530

    change to coockie to headers

--- db/db.js
+++ db/db.js
@@ -1,9 +1,9 @@
 const mysql=require("mysql")
 require("dotenv").config()
 const dbConfig = {
-  host: process.env.host,
-  user: process.env.database_user,
-  password: process.env.database_password,
+  host: "localhost",
+  user: "root",
+  password: "Suvam@7787",
   database: process.env.database_name,
   connectionLimit: 100,
 
