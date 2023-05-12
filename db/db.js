d8bfa4bb487a0d70f1f69cf7d11beb14f968449f
Suvam Swagatam panda <suvampandar@gmail.com>
2023-05-12 14:41:34 +0530

    update user todo routes

--- db/db.js
+++ db/db.js
@@ -1,9 +1,9 @@
 const mysql=require("mysql")
 require("dotenv").config()
 const dbConfig = {
-  host: "localhost",
-  user: "root",
-  password: "Suvam@7787",
+  host: process.env.host,
+  user: process.env.database_user,
+  password: process.env.database_user, 
   database: process.env.database_name,
   connectionLimit: 100,
 
